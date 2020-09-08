12e3ec95df8124eb989ab415d7b3ca95ef42ba98
joycenayanna <joycenayanna@gmail.com>
2020-09-07 21:56:21 -0400

    main update

--- main.cpp
+++ main.cpp
@@ -1,17 +1,43 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "score.h"
 
 using namespace std;
 
 int main() {
 	ifstream lecteur("hiscore.txt");
+	ofstream ecrire("asterostorm.txt");
+
 	string line;
+	string data[3];
+	vector<Score> scores;
+	int i = 0;
 
 	while(!lecteur.eof()) {
 		lecteur >> line;
-		if(!lecteur.eof()) {
-			cout << line << endl;
+		int rem = i%3;
+		data[rem] = line;
+		if(rem == 2) {
+			Score score(data[0], data[1], data[2]);
+			scores.push_back(score);
 		}
+		++i;
 	}
+
+	sort(scores.begin(), scores.end(), 
+		[](Score& lhs, Score& rhs) {
+    		return lhs.getPoints() > rhs.getPoints();
+		}
+	);
+
+	for(int i=0 ; i < scores.size() ; i++) {
+		ecrire << scores[i].getUsername() + '\n';
+		ecrire << scores[i].getId() + '\n';
+		ecrire << "Asterostorm=" + to_string(scores[i].getPoints()) + '\n';
+	}
+
 	lecteur.close();
 }
